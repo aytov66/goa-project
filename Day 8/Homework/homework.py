134b1d93e5be4886f1ca36469509f0a8d241ec91
aytov66 <hacker.exe2143@gmail.com>
2025-05-19 12:14:25 +0400

    second commit

--- Day 8/Homework/homework.py
+++ Day 8/Homework/homework.py
@@ -9,3 +9,4 @@ print()
 for i in range(0, 10, 2):  # if we want the 10 to be included, we can do range(0, 12, 2)
     print(i)
     i += 2
+# a for loop repeats selected code a certain amount of times using a range (start, stop ,step)
