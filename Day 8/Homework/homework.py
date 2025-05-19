76525eaeeb3d09efff37ab0ee7208e9ebc64f315
aytov66 <hacker.exe2143@gmail.com>
2025-05-19 12:08:11 +0400

    second commit

---
+++ Day 8/Homework/homework.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    print(i + 1)
+    i += 1
+print()
+for i in range(5):
+    print("Hello World")
+    i += 1
+print()
+for i in range(0, 10, 2):  # if we want the 10 to be included, we can do range(0, 12, 2)
+    print(i)
+    i += 2
